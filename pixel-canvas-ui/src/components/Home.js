dbed8fb6db0a1e19b4920db4eb7bc877bc2b2d9e
NicholasCappo <Nicholas.Cappo@Target.com>
2022-01-03 12:36:01 -0600

    feat: home messages

--- pixel-canvas-ui/src/components/Home.js
+++ pixel-canvas-ui/src/components/Home.js
@@ -3,7 +3,7 @@ import CanvasPreview from './CanvasPreview'
 import NewCanvas from './NewCanvas'
 
 const Home = () => {
-  const [canvasList, setCanvasList] = useState([])
+  const [canvasList, setCanvasList] = useState(null)
   // get canvas
   useEffect(() => {
     const getCanvas = async () => {
@@ -26,8 +26,10 @@ const Home = () => {
             <NewCanvas addCanvas={addCanvas} />
           </div>
           <div id="gallery-list" className="relative w-full">
+            {!canvasList && <h2 className="text-3xl font-extrabold text-gray-400 w-full text-center animate-pulse">Loading...</h2>}
+            {canvasList?.length === 0 && <h2 className="text-3xl font-extrabold text-gray-400 w-full text-center">Empty</h2>}
             <div  className="xs:pl-[40%] md:pl-[50%] w-full flex gap-20 snap-x snap-mandatory overflow-auto pb-14 pr-[50%] pt-10">
-            {canvasList.map((canvas) => (
+            {canvasList && canvasList.map((canvas) => (
               <div key={canvas._id} className="snap-center shrink-0">
                 <CanvasPreview  canvas={canvas} />
               </div>
